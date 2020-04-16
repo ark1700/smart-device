84c45059cde5d585b8fc9a01e003425c425384ff
ark1700 <45306658+ark1700@users.noreply.github.com>
2020-04-16 21:53:19 +0300

    Добавил footer

--- build/js/script.js
+++ build/js/script.js
@@ -1,15 +1,35 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
 
-pageHeader.classList.remove('page-header--nojs');
+var footerNavTitle = document.querySelector('.footer__title-nav');
+var footerNav = document.querySelector('.footer__navigation');
+var footerContactsTitle = document.querySelector('.footer__title-contacts');
+var footerContacts = document.querySelector('.footer__contacts');
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
-  }
-});
+if (footerNav) {
+  footerNav.classList.add('footer__navigation--m-hide');
+}
+if (footerContacts) {
+  footerContacts.classList.add('footer__contacts--m-hide');
+}
+
+if (footerNavTitle) {
+  footerNavTitle.addEventListener('click', function () {
+    if (footerNav.classList.contains('footer__navigation--m-hide')) {
+      footerNav.classList.remove('footer__navigation--m-hide');
+    } else {
+      footerNav.classList.add('footer__navigation--m-hide');
+    }
+  });
+}
+
+if (footerContactsTitle) {
+  footerContactsTitle.addEventListener('click', function () {
+    if (footerContacts.classList.contains('footer__contacts--m-hide')) {
+      footerContacts.classList.remove('footer__contacts--m-hide');
+    } else {
+      footerContacts.classList.add('footer__contacts--m-hide');
+    }
+  });
+}
+
+
--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,29 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
 
-pageHeader.classList.remove('page-header--nojs');
+var footerNavTitle = document.querySelector('.footer__title-nav');
+var footerNav = document.querySelector('.footer__navigation');
+var footerContactsTitle = document.querySelector('.footer__title-contacts');
+var footerContacts = document.querySelector('.footer__contacts');
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
+if (footerNav) {
+  footerNav.classList.add('footer__navigation--m-hide');
+}
+if (footerContacts) {
+  footerContacts.classList.add('footer__contacts--m-hide');
+}
+
+footerNavTitle.addEventListener('click', function () {
+  if (footerNav.classList.contains('footer__navigation--m-hide')) {
+    footerNav.classList.remove('footer__navigation--m-hide');
   } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
+    footerNav.classList.add('footer__navigation--m-hide');
+  }
+});
+
+footerContactsTitle.addEventListener('click', function () {
+  if (footerContacts.classList.contains('footer__contacts--m-hide')) {
+    footerContacts.classList.remove('footer__contacts--m-hide');
+  } else {
+    footerContacts.classList.add('footer__contacts--m-hide');
   }
 });
